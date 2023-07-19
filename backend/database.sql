b45047a948082b05958c415491b9850488388163
JIMI_BES <jimi.bes@gmail.com>
2023-07-19 10:57:04 +0200

    database initialized

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,39 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE
+    IF NOT EXISTS Stone (
+        id INT AUTO_INCREMENT,
+        name VARCHAR(255),
+        description TEXT,
+        color VARCHAR(255),
+        origin VARCHAR(255),
+        hardness INT,
+        image_url VARCHAR(255),
+        PRIMARY KEY(id)
+    );
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+CREATE TABLE
+    IF NOT EXISTS User (
+        id INT AUTO_INCREMENT,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        password_hash VARCHAR(255),
+        role VARCHAR(255),
+        PRIMARY KEY(id)
+    );
+
+INSERT INTO
+    Stone (
+        name,
+        description,
+        color,
+        origin,
+        hardness,
+        image_url
+    )
+VALUES (
+        'Ruby',
+        'A red precious gemstone that is one of the traditional cardinal gems.',
+        'Red',
+        'Myanmar',
+        9,
+        'https://example.com/images/ruby.jpg'
+    );
